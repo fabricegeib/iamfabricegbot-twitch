b4bcf2a3353f30d43e9b8a33d82cead71f5a5e27
Fabrice Geib <fabricegeib@gmail.com>
2020-11-25 13:23:29 +0100

    Update reactions.js

--- reactions.js
+++ reactions.js
@@ -1,7 +1,8 @@
 const reactions = {
-  "trade" : "Oh mon dieu ! RuleFive",
-  "wesh" : "Wesh alors Wesh alors wesh alors ! Kappa",
-  "epic" : "Mon pseudo EPIC Games : iamfabriceg"
+  "trade": "Oh mon dieu ! RuleFive",
+  "wesh": "Wesh alors Wesh alors wesh alors ! Kappa",
+  "epic": "Mon pseudo EPIC Games : iamfabriceg",
+  "bites": `@${tags.username} calme toi ou je te calme`
 }
 
 module.exports = reactions;
